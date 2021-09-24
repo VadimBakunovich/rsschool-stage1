242a33261b95686a0063b33c8506c3fa46d67dae
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-09-24 22:31:33 +0300

    feat: add tickets section, parallax and contacts

--- museum/scripts/gallery.js
+++ museum/scripts/gallery.js
@@ -37,7 +37,7 @@ const fillGallery = _ => {
   const shuffle = arr => {
     for (let i = arr.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+      [arr[i], arr[j]] = [arr[j], arr[i]];
     }
   }
 
@@ -45,25 +45,49 @@ const fillGallery = _ => {
   shuffle(idxs);
 
   let gallery = [];
+
   for (let i of idxs) {
-    const item = document.createElement('img');
-    item.className = 'gallery__item';
-    item.src = `assets/img/galery/galery${i}.jpg`;
-    item.alt = `tile-${i}`;
-    if (item.width > item.height) item.classList.add('horizo');
-    else if (item.width == item.height) item.classList.add('square');
-    else if (item.width < item.height) item.classList.add('vertic');
-    gallery.push(item);
+    const img = document.createElement('img');
+    img.className = 'gallery__item';
+    img.alt = `tile-${i}`;
+
+    switch (i) {
+      case 1 : img.src = 'assets/img/galery/1square.jpg'; break;
+      case 2 : img.src = 'assets/img/galery/2vertic.jpg'; break;
+      case 3 : img.src = 'assets/img/galery/3vertic.jpg'; break;
+      case 4 : img.src = 'assets/img/galery/4square.jpg'; break;
+      case 5 : img.src = 'assets/img/galery/5vertic.jpg'; break;
+      case 6 : img.src = 'assets/img/galery/6vertic.jpg'; break;
+      case 7 : img.src = 'assets/img/galery/7vertic.jpg'; break;
+      case 8 : img.src = 'assets/img/galery/8vertic.jpg'; break;
+      case 9 : img.src = 'assets/img/galery/9vertic.jpg'; break;
+      case 10 : img.src = 'assets/img/galery/10square.jpg'; break;
+      case 11 : img.src = 'assets/img/galery/11square.jpg'; break;
+      case 12 : img.src = 'assets/img/galery/12horizo.jpg'; break;
+      case 13 : img.src = 'assets/img/galery/13horizo.jpg'; break;
+      case 14 : img.src = 'assets/img/galery/14vertic.jpg'; break;
+      case 15 : img.src = 'assets/img/galery/15square.jpg'; break;
+      default : break;
+    }
+
+    if (img.src.includes('horizo')) img.classList.add('horizo');
+    else if (img.src.includes('square')) img.classList.add('square');
+    else if (img.src.includes('vertic')) img.classList.add('vertic');
+
+    gallery.push(img);
   }
-  
+
+  const container = document.querySelector('.gallery__inner');
+
   for (let i of templates[0]) {
     for (let j of gallery) {
       if (j.classList.contains(i) || i == 'others') {
-        inner.append(j);
+        container.append(j);
         gallery = gallery.filter(el => el.src != j.src);
         break;
       } else continue;
     }
   }
 }
+
 fillGallery();
