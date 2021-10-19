6c1cc7faf7e98e65b10fbb44bb20a430a9a3d2d2
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-10-19 13:41:09 +0300

    feat: add background slider

---
+++ momentum/scripts/bgSlider.js
@@ -0,0 +1,27 @@
+export function bgSlider() {
+  const getTimeOfDay = _ => {
+    const hours = new Date().getHours();
+    if (hours < 6) return 'night';
+    else if (hours < 12) return 'morning';
+    else if (hours < 18) return 'afternoon';
+    else return 'evening';
+  }
+  let randomNum = Math.floor(Math.random() * 20) + 1;
+
+  const setBg = (timeOfDay, bgNum) => {
+    bgNum = `${bgNum}`.padStart(2, '0');
+    const img = new Image();
+    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.onload = _ => body.style.backgroundImage = `url(${img.src})`;
+  }
+  setBg(getTimeOfDay(), randomNum);
+
+  sliderPrev.onclick = _ => {
+    randomNum === 1 ? randomNum = 20 : randomNum -= 1;
+    setBg(getTimeOfDay(), randomNum);
+  }
+  sliderNext.onclick = _ => {
+    randomNum === 20 ? randomNum = 1 : randomNum += 1;
+    setBg(getTimeOfDay(), randomNum);
+  }
+}
