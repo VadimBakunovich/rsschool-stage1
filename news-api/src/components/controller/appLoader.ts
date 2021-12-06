58bb92429065865c421459901040c9294f32b792
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-06 22:43:06 +0300

    fix: change url in appLoader.ts

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: 'c49d25c9f6de4b27b30b25eedb63391e', // получите свой ключ https://newsapi.org/
     });
   }
