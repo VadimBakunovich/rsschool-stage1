1a1a24c306f1422951050cfd356e004b88db8768
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-06 19:53:09 +0300

    fix: add types in loader.ts

--- news-api/src/components/controller/loader.ts
+++ news-api/src/components/controller/loader.ts
@@ -1,46 +1,48 @@
+import { IOptions, IData, Callback } from '../app/types';
+
 class Loader {
-    constructor(baseLink, options) {
-        this.baseLink = baseLink;
-        this.options = options;
+  baseLink: string
+  options: IOptions
+
+  constructor(baseLink: string, options: IOptions) {
+    this.baseLink = baseLink;
+    this.options = options;
+  }
+
+  getResp(
+    { endpoint = '', options = {} },
+    callback: Callback,
+  ) {
+    this.load('GET', endpoint, callback, options);
+  }
+
+  errorHandler = (res: Response): Response => {
+    if (!res.ok) {
+      if (res.status === 401 || res.status === 404) { console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`); }
+      throw Error(res.statusText);
     }
 
-    getResp(
-        { endpoint, options = {} },
-        callback = () => {
-            console.error('No callback for GET response');
-        }
-    ) {
-        this.load('GET', endpoint, callback, options);
-    }
+    return res;
+  }
 
-    errorHandler(res) {
-        if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
-                console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
-            throw Error(res.statusText);
-        }
+  makeUrl(options: IOptions, endpoint: string) {
+    const urlOptions = { ...this.options, ...options };
+    let url = `${this.baseLink}${endpoint}?`;
 
-        return res;
-    }
+    Object.keys(urlOptions).forEach(key => {
+      url += `${key}=${urlOptions[key]}&`;
+    });
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
-        let url = `${this.baseLink}${endpoint}?`;
+    return url.slice(0, -1);
+  }
 
-        Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
-        });
-
-        return url.slice(0, -1);
-    }
-
-    load(method, endpoint, callback, options = {}) {
-        fetch(this.makeUrl(options, endpoint), { method })
-            .then(this.errorHandler)
-            .then((res) => res.json())
-            .then((data) => callback(data))
-            .catch((err) => console.error(err));
-    }
+  load(method: string, endpoint: string, callback: Callback, options = {}) {
+    fetch(this.makeUrl(options, endpoint), { method })
+      .then(this.errorHandler)
+      .then(res => res.json())
+      .then((data: IData) => callback(data))
+      .catch((err: Error) => console.error(err));
+  }
 }
 
 export default Loader;
