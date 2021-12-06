8af1d54b19563a3ac2abcc9f8c20dc7813a84c00
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-06 19:59:41 +0300

    fix: add types in sources.ts

--- news-api/src/components/view/sources/sources.ts
+++ news-api/src/components/view/sources/sources.ts
@@ -1,21 +1,22 @@
+import { IObj } from '../../app/types';
 import './sources.css';
 
 class Sources {
-    draw(data) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp');
+  draw(data: IObj[]) {
+    const fragment = document.createDocumentFragment();
+    const sourceItemTemp: HTMLTemplateElement = document.querySelector('#sourceItemTemp');
 
-        data.forEach((item) => {
-            const sourceClone = sourceItemTemp.content.cloneNode(true);
+    data.forEach(item => {
+      const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
 
-            sourceClone.querySelector('.source__item-name').textContent = item.name;
-            sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
+      sourceClone.querySelector('.source__item-name').textContent = item.name;
+      sourceClone.querySelector('.source__item').setAttribute('data-source-id', item.id);
 
-            fragment.append(sourceClone);
-        });
+      fragment.append(sourceClone);
+    });
 
-        document.querySelector('.sources').append(fragment);
-    }
+    document.querySelector('.sources').append(fragment);
+  }
 }
 
 export default Sources;
