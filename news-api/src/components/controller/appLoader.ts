e1de59ca0b49c68d10f4eee429f55ab50b776729
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-06 19:54:03 +0300

    fix: add types in appLoader.ts

--- news-api/src/components/controller/appLoader.ts
+++ news-api/src/components/controller/appLoader.ts
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: 'c49d25c9f6de4b27b30b25eedb63391e', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
