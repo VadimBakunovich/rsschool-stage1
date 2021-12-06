3041ff4c6eaf90d0a7de48329f7cae208cb4815f
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-12-06 22:50:44 +0300

    feat: add files for deploy

---
+++ news-api/test/components/controller/controller.ts
@@ -0,0 +1,40 @@
+import { Callback } from '../app/types';
+import AppLoader from './appLoader';
+
+class AppController extends AppLoader {
+  getSources(callback: Callback) {
+    super.getResp(
+      {
+        endpoint: 'sources',
+      },
+      callback,
+    );
+  }
+
+  getNews(e: Event, callback: Callback) {
+    let target = e.target as Element;
+    const newsContainer = e.currentTarget as Element;
+
+    while (target !== newsContainer) {
+      if (target.classList.contains('source__item')) {
+        const sourceId: string = target.getAttribute('data-source-id');
+        if (newsContainer.getAttribute('data-source') !== sourceId) {
+          newsContainer.setAttribute('data-source', sourceId);
+          super.getResp(
+            {
+              endpoint: 'everything',
+              options: {
+                sources: sourceId,
+              },
+            },
+            callback,
+          );
+        }
+        return;
+      }
+      target = target.parentNode as Element;
+    }
+  }
+}
+
+export default AppController;
